d1c6d83c2eab1a3122d6fc7405a92c7335e285b1
김유열 <kyyyy8629@gmail.com>
2024-04-21 23:06:28 +0900

    second commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express= require('express');
+const { default: mongoose }= require('mongoose');
+const app= express();
+
+app.use(express.json());
+// form태그에서 전달받은 값을 받기위한 미들웨어
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect(``)
+
+
+
+
+
+
+
+
+const PORT= 4000;
+app.listen(PORT, ()=> {
+    console.log(`listening on port ${PORT}`);
+})
