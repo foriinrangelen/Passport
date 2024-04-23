7fd0abf5297d595bdf340ef17635e48aa3e57b08
foriinrangelen <123726292+foriinrangelen@users.noreply.github.com>
2024-04-23 19:17:45 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,6 +16,4 @@ mongoose.connect(``)
 
 
 const PORT= 4000;
-app.listen(PORT, ()=> {
-    console.log(`listening on port ${PORT}`);
-})
+app.listen(PORT, ()=> console.log(`listening on port ${PORT}`); );
