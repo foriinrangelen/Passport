a8e4cba61b308c7bd6e79d09ae074473ac14de06
김유열 <kyyyy8629@gmail.com>
2024-05-12 12:37:01 +0900

    mongoose 설정

--- src/server.js
+++ src/server.js
@@ -1,21 +1,25 @@
-const express= require('express');
-const { default: mongoose }= require('mongoose');
-const app= express();
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const path = require("path");
+const app = express();
 
 app.use(express.json());
 // form태그에서 전달받은 값을 받기위한 미들웨어
 app.use(express.urlencoded({ extended: false }));
 
-mongoose.connect(``)
-
-
-
-
-
-
-
-
-const PORT= 4000;
-app.listen(PORT, ()=> {
-    console.log(`listening on port ${PORT}`);
-})
+mongoose
+  .connect(
+    `mongodb+srv://kyyyy8629:1234@express-cluster.fzshmg3.mongodb.net/?retryWrites=true&w=majority&appName=express-Cluster`
+  )
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.use("/static", express.static(path.join(__dirname, "..", "public")));
+console.log(path.join(__dirname, "..", "public"));
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
